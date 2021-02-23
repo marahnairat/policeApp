cee1684f85f28facf612a99cf0dfbea1bc5ffb2a
marahnairat <48821626+marahnairat@users.noreply.github.com>
2021-02-23 22:49:00 +0200

    hh

--- app/src/main/java/com/example/policeapp/MainActivity.java
+++ app/src/main/java/com/example/policeapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.policeapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        Button yolobtn;
+        yolobtn=findViewById(R.id.yolobtn);
+        yolobtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(android.view.View view) {
+            runYOLOAlgo();
+            }
+
+            private void runYOLOAlgo() {
+            }
+        });
+    
 }
+    }
